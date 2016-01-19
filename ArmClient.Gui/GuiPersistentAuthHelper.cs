6ee3956374793b4682546812dd74ff69b0aa69b0
Cynthia-Jiang <yayago_up@outlook.com>
2016-01-19 15:16:54 +0800

    upload Arm gui

---
+++ ArmClient.Gui/GuiPersistentAuthHelper.cs
@@ -0,0 +1,18 @@
+﻿using ARMClient.Authentication.AADAuthentication;
+using ARMClient.Authentication.Contracts;
+using System.Collections.Generic;
+
+namespace ArmGuiClient
+{
+    public class GuiPersistentAuthHelper : PersistentAuthHelper
+    {
+        public GuiPersistentAuthHelper()
+        {
+        }
+
+        public Dictionary<string, TenantCacheInfo> GetTenants()
+        {
+            return this.TenantStorage.GetCache();
+        }
+    }
+}
